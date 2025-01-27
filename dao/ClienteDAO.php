3c96c47ea526644f419767feaac1646ea639b3a0
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2025-01-27 13:57:19 -0300

    Update ClienteDAO.php

--- dao/ClienteDAO.php
+++ dao/ClienteDAO.php
@@ -79,22 +79,24 @@
             $stm = $con->prepare($sql);
             $stm->execute(array($id));
             $registro = $stm->fetch();
-        
-            if($registro['tipo'] == 'F') {
-                $cliente = new ClientePF();
-                $cliente->setNome($registro['nome']);
-                $cliente->setCpf($registro['cpf']);
-            } else {
-                $cliente = new ClientePJ();
-                $cliente->setRazaoSocial($registro['razao_social']);
-                $cliente->setCnpj($registro['cnpj']);
+            
+            if ($registro) {
+                $cliente = null;
+                if($registro['tipo'] == 'F') {
+                    $cliente = new ClientePF();
+                    $cliente->setNome($registro['nome']);
+                    $cliente->setCpf($registro['cpf']);
+                } else {
+                    $cliente = new ClientePJ();
+                    $cliente->setRazaoSocial($registro['razao_social']);
+                    $cliente->setCnpj($registro['cnpj']);
+                }
+            
+                $cliente->setId($registro['id']);
+                $cliente->setNomeSocial($registro['nome_social']);
+                $cliente->setEmail($registro['email']);
+                    return $cliente;
+                }
             }
-        
-            $cliente->setId($registro['id']);
-            $cliente->setNomeSocial($registro['nome_social']);
-            $cliente->setEmail($registro['email']);
-                return $cliente;
-            }
+            
         }
-
-    }
